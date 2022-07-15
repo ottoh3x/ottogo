5ad3522aff3f562d68415f605f5a7f0ff181e319
ottoh3x <70418756+ottoh3x@users.noreply.github.com>
2022-07-15 14:39:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,17 @@
-import uvicorn
 from fastapi import FastAPI
-from .gogoanime import *
-import json
+import datetime
 
 app = FastAPI()
+
+@app.get("/example/{parameter}")
+def example(parameter: str):
+    return {
+        "parameter": parameter,
+        "datetime": datetime.datetime.now().time()
+    }
 
 @app.get("/")
 def main():
     return {
-        "message": "Hello friend"
+        "message": "Hello my friend"
     }
-
-@app.get('/api/recently/{page}')
-def recently(page: int):
-    recently = GogoanimeParser.get_recently_uploaded(page=page)
-    return json.loads(recently)
-
-
-@app.get('/api/popular/{page}')
-def popular(page: int):
-    popular = GogoanimeParser.popular(page=page)
-    return json.loads(popular)
-
-
-@app.get('/api/genre/{genre}/{page}')
-def genre(genre: str, page: int):
-    genre = GogoanimeParser.genre(genre_name=genre, page=page)
-    return genre
-
-
-@app.get('/api/details/{animeid}')
-def details(animeid: str):
-    detail = GogoanimeParser.details(animeid=animeid)
-    return detail
-
-
-@app.get('/api/{id}/{episode_num}')
-def episode(id: str, episode_num: int):
-    episode = GogoanimeParser.episode(animeid=id, episode_num=episode_num)
-    return episode
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
