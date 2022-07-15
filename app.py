0a9192439709ae79cf90cdde1813649d524169ec
ottoh3x <70418756+ottoh3x@users.noreply.github.com>
2022-07-15 14:41:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 import datetime
+from .gogoanime import *
+import json
 
 app = FastAPI()
 
-@app.get("/example/{parameter}")
-def example(parameter: str):
-    return {
-        "parameter": parameter,
-        "datetime": datetime.datetime.now().time()
-    }
+@app.get('/recently/{page}')
+def recently(page: int):
+    recently = GogoanimeParser.get_recently_uploaded(page=page)
+    return json.loads(recently)
 
 @app.get("/")
 def main():
